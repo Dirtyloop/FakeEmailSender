7b1dca2f27e0e9014a5f3164330100433887018d
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-02 19:01:48 +0100

    initial commit

---
+++ src/main/java/com/komfortcieplny/FakeEmailSender/FakeEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.FakeEmailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komfortcieplny/FakeEmailSender/FakeEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komfortcieplny.FakeEmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
