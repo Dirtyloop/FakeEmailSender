7cff392afe3d7bf9d696675879f60f000d516d03
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-03 00:19:36 +0100

    logfile added

--- src/main/java/com/komfortcieplny/FakeEmailSender/user/controller/UserController.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.komfortcieplny.FakeEmailSender.user.controller;
 
 import com.komfortcieplny.FakeEmailSender.user.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,15 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class UserController {
 
+    private static final Logger logger = LogManager.getLogger(UserController.class);
+    private List<User> userList = new ArrayList<>();
+
+
     @GetMapping("/users")
     public List<User> getUsers() {
-        List<User> userList = new ArrayList<>();
         userList.add(new User("Michal", "michal.nowakowski85@gmail.com"));
         userList.add(new User("Joanna", "zmyslonyadres@pl"));
+        logger.info("Users displayed");
         return userList;
     }
 }
