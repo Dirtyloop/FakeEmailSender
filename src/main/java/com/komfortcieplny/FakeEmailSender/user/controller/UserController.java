54d656377f8743149788be5e2b7e31b651264318
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-05 09:06:44 +0100

    updateUser and deleteUser added

--- src/main/java/com/komfortcieplny/FakeEmailSender/user/controller/UserController.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/user/controller/UserController.java
@@ -7,8 +7,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -39,7 +41,29 @@ public class UserController {
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = userService.createUser(user);
-        logger.info("User with name " + user.getName() + " created");
+        logger.info("User with name " + newUser.getName() + " created");
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("id") Long id) {
+        if (user.getId()==null) {
+            user.setId(id);
+        }
+        if (Objects.equals(user.getId(), id)) {
+            User updatedUser = userService.updateUser(user);
+            logger.info("User with id " + updatedUser.getId() + " updated");
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        }
+        logger.info("User with id " + id + " can not be updated with id " + user.getId());
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        logger.info("User with id " + id + " deleted");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/komfortcieplny/FakeEmailSender/user/service/UserService.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/user/service/UserService.java
@@ -28,4 +28,12 @@ public class UserService {
     public User createUser(User user) {
         return userRepository.save(user);
     }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
