f6b5c970e7777cee85e0d6c99836620b86acd4fe
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-06 14:32:57 +0100

    sendEmail to one user added

--- src/main/java/com/komfortcieplny/FakeEmailSender/email/controller/EmailController.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/email/controller/EmailController.java
@@ -2,55 +2,52 @@ package com.komfortcieplny.FakeEmailSender.email.controller;
 
 import com.komfortcieplny.FakeEmailSender.email.model.EmailModel;
 import com.komfortcieplny.FakeEmailSender.email.service.EmailService;
-import com.komfortcieplny.FakeEmailSender.user.controller.UserController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import com.komfortcieplny.FakeEmailSender.utils.RequestLogger;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/email")
 public class EmailController {
 
-    private static final Logger logger = LogManager.getLogger(UserController.class);
-
-    private final EmailModel emailModel = new EmailModel(
-            "michal.nowakowski85@gmail.com",
-            "temat",
-            "Witaj! Otrzymałeś pierwszy email z FakeEmailSender!");
-
     private final EmailService emailService;
+    private final RequestLogger requestLogger;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, RequestLogger requestLogger) {
         this.emailService = emailService;
+        this.requestLogger = requestLogger;
     }
 
-    @RequestMapping("/hello")
-    public String hello() {
-        //emailService.sendEmail(emailModel);
-        return "Hello!";
-    }
-
-    @PostMapping("/send")
-    public String send(@RequestBody EmailModel emailModel) {
+    @PostMapping("/")
+    public String sendEmail(@RequestBody EmailModel emailModel) {
+        try {
+            requestLogger.logInfo(
+                    String.format(
+                            "sendEmail to all users with %s requested",
+                            emailModel.toString()
+                    )
+            );
+        } catch (IOException e) {
+            System.out.println("Exception! sendEmail request was not logged");
+        }
         emailService.sendEmail(emailModel);
-        logger.info("sent controller");
-        return "Email2 sent";
+        return "Email sent to all users";
     }
 
-    @GetMapping("/sendemail")
-    public String sendEmail() {
-        //emailService.sendEmail(emailModel);
-        logger.info("sent controller");
-        return "Email sent";
+    @PostMapping("/{id}")
+    public String sendEmail(@RequestBody EmailModel emailModel, @PathVariable("id") Long id) {
+        try {
+            requestLogger.logInfo(
+                    String.format(
+                            "sendEmail to user with id %d with %s requested",
+                            id,
+                            emailModel.toString()
+                    )
+            );
+        } catch (IOException e) {
+            System.out.println("Exception! sendEmail request was not logged");
+        }
+        emailService.sendEmailToId(emailModel, id);
+        return String.format("Email sent to user with id %d", id);
     }
-    @PostMapping("/sendemail2")
-    public String sendEmail2(@RequestBody EmailModel emailModel) {
-        emailService.sendEmail(emailModel);
-        logger.info("sent controller");
-        return "Email2 sent";
-    }
-
 }
