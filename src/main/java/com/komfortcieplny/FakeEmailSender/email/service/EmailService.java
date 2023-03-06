b47343814e94113889eaba443332740580ca9e42
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-06 14:31:31 +0100

    EmailService cleaned and updated

--- src/main/java/com/komfortcieplny/FakeEmailSender/email/service/EmailService.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/email/service/EmailService.java
@@ -1,62 +1,56 @@
 package com.komfortcieplny.FakeEmailSender.email.service;
 
 import com.komfortcieplny.FakeEmailSender.email.model.EmailModel;
-import com.komfortcieplny.FakeEmailSender.email.utils.EmailLog;
+import com.komfortcieplny.FakeEmailSender.user.controller.UserController;
 import com.komfortcieplny.FakeEmailSender.user.model.User;
 import com.komfortcieplny.FakeEmailSender.user.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EmailService {
-
+    private static final String FROM = "FakeEmailSender <javafakeapp@gmail.com>";
+    private static final Logger logger = LogManager.getLogger(UserController.class);
     private final JavaMailSender mailSender;
-    private final EmailLog emailLog = new EmailLog();
     private final UserService userService;
 
     public EmailService(JavaMailSender mailSender, UserService userService) {
         this.mailSender = mailSender;
         this.userService = userService;
     }
-    public void sendEmail(EmailModel emailModel){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        String[] emailList = prepareEmailList();
-        mailMessage.setFrom("FakeEmailSender");
-        mailMessage.setTo(emailList);
-        mailMessage.setSubject(emailModel.subject());
-        mailMessage.setText(emailModel.message());
-        mailSender.send(mailMessage);
-        try {
-            emailLog.logInfo("Message to " + emailModel.recipient() + " sent");
-        } catch (IOException e) {
-            System.out.println("Exception! Message was not logged");
-        }
-        System.out.println("sent");
+
+    public void sendEmail(EmailModel emailModel) {
+        String[] emails = prepareEmailList();
+        mailSender.send(prepareMailMessage(emails, emailModel));
+        logger.info("Email sent to all users");
+    }
+
+    public void sendEmailToId(EmailModel emailModel, Long id) {
+        String[] emails = {userService.getUser(id).getEmail()};
+        mailSender.send(prepareMailMessage(emails, emailModel));
+        logger.info(String.format("Email sent to user with id %d", id));
     }
 
     private String[] prepareEmailList() {
         List<User> userList = userService.getUsers();
         List<String> emailList = new ArrayList<>();
-        for(User user : userList) {
+        for (User user : userList) {
             emailList.add(user.getEmail());
         }
         return emailList.toArray(new String[0]);
     }
 
-    public void sendE() {
+    private SimpleMailMessage prepareMailMessage(String[] emails, EmailModel emailModel) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("FakeEmailSender");
-        mailMessage.setTo("michal.nowakowski85@gmail.com");
-        mailMessage.setSubject("Subject");
-        mailMessage.setText("Text");
-
-        mailSender.send(mailMessage);
-
-        System.out.println("sendE");
+        mailMessage.setFrom(FROM);
+        mailMessage.setTo(emails);
+        mailMessage.setSubject(emailModel.subject());
+        mailMessage.setText(emailModel.message());
+        return mailMessage;
     }
 }
