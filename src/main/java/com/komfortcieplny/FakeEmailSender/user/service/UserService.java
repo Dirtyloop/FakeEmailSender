0fdd472e497ea7b03ae8a5e4cc845ef07f8675a5
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-06 13:03:35 +0100

    UserService logger updated

--- src/main/java/com/komfortcieplny/FakeEmailSender/user/service/UserService.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/user/service/UserService.java
@@ -6,14 +6,12 @@ import com.komfortcieplny.FakeEmailSender.user.repository.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class UserService {
 
     private static final Logger logger = LogManager.getLogger(UserService.class);
-
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -29,24 +27,24 @@ public class UserService {
     public User getUser(Long id) {
         User user = userRepository.findById(id).orElseThrow(() ->
                 new UserNotFoundException(String.format("User with id %s was not found", id)));
-        logger.info("User with id " + id + " displayed");
+        logger.info(String.format("User with id %s displayed", id));
         return user;
     }
 
     public User createUser(User user) {
         User newUser = userRepository.save(user);
-        logger.info("User with id " + newUser.getId() + " created");
+        logger.info(String.format("User with id %s created", newUser.getId()));
         return newUser;
     }
 
     public User updateUser(User user) {
         User updatedUser = userRepository.save(user);
-        logger.info("User with id " + updatedUser.getId() + " updated");
+        logger.info(String.format("User with id %s updated", updatedUser.getId()));
         return updatedUser;
     }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
-        logger.info("User with id " + id + " deleted");
+        logger.info(String.format("User with id %s deleted", id));
     }
 }
