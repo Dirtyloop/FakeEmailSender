7ae3e7474c6405cf103a4bcc7b13df6aa5cfe723
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-08 22:24:25 +0100

    EmailServiceTest with prepare emails and sendEmail test added

--- src/test/java/com/komfortcieplny/FakeEmailSender/email/service/EmailServiceTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/email/service/EmailServiceTest.java
@@ -42,6 +42,22 @@ class EmailServiceTest {
     }
 
     @Test
-    void sendEmailToId() {
+    @DisplayName("Should prepare emails and Invoke mailSender.send")
+    void shouldPrepareEmailsAndSendTest() {
+        EmailModel emailModel = new EmailModel("Subject", "Message");
+        String [] emails = {"adam@test.com", "ewa@test.com", "ola@test.com"};
+        User user1 = new User(1l, "Adam", "adam@test.com");
+        User user2 = new User(2l, "Ewa", "ewa@test.com");
+        User user3 = new User(3l, "Ola", "ola@test.com");
+
+        when(userService.getUsers()).thenReturn(List.of(user1, user2, user3));
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("FakeEmailSender <javafakeapp@gmail.com>");
+        mailMessage.setTo(emails);
+        mailMessage.setSubject(emailModel.subject());
+        mailMessage.setText(emailModel.message());
+        emailService.sendEmail(emailModel);
+
+        verify(mailSender, times(1)).send(mailMessage);
     }
 }
