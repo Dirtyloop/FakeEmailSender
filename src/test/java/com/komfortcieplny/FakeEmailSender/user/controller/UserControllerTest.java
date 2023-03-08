86519233ed9b1f1a028bab11e3a8394871c73a77
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-09 00:07:50 +0100

    UserControllerTest imports updated

--- src/test/java/com/komfortcieplny/FakeEmailSender/user/controller/UserControllerTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/user/controller/UserControllerTest.java
@@ -2,16 +2,13 @@ package com.komfortcieplny.FakeEmailSender.user.controller;
 
 import com.komfortcieplny.FakeEmailSender.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
