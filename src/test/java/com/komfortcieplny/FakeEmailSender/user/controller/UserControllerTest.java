e5a003c949e0e171a6c7aeee97282b20eaaed585
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-09 00:06:05 +0100

    UserControllerTest @DirtiesContext added

--- src/test/java/com/komfortcieplny/FakeEmailSender/user/controller/UserControllerTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/user/controller/UserControllerTest.java
@@ -2,11 +2,14 @@ package com.komfortcieplny.FakeEmailSender.user.controller;
 
 import com.komfortcieplny.FakeEmailSender.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.aMapWithSize;
@@ -18,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class UserControllerTest {
 
     @Autowired
