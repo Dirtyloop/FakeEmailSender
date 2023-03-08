114e1c6e87409cdd090a64b63d8736b73c324602
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-08 12:03:18 +0100

    userRepository 4 tests added

--- src/test/java/com/komfortcieplny/FakeEmailSender/user/repository/UserRepositoryTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/user/repository/UserRepositoryTest.java
@@ -34,4 +34,69 @@ public class UserRepositoryTest {
         assertEquals(expectedName, user5.getName(), "User with id 5 have name Joanna");
     }
 
+    @Test
+    @DisplayName("newUser Saved In Database Should Have Id=6")
+    @Order(3)
+    public void saveNewUserToDatabaseTest() {
+        User newUser = User.builder()
+                .name("Boguś")
+                .email("bogus@example.com")
+                .build();
+        Long expectedId = 6l;
+
+        userRepository.save(newUser);
+
+        assertEquals(expectedId, newUser.getId(), "newUser saved in database have id 6");
+    }
+
+    @Test
+    @DisplayName("Another newUser Saved In Database Should Have Id=7")
+    @Order(4)
+    public void saveAnotherNewUserToDatabaseTest() {
+        User newUser2 = User.builder()
+                .name("Boguslaw")
+                .email("bogus@example.com")
+                .build();
+        Long expectedId2 = 7l;
+
+        userRepository.save(newUser2);
+
+        assertEquals(expectedId2, newUser2.getId(), "Another newUser saved in database have id 7");
+    }
+
+    @Test
+    @DisplayName("Should Throw UserNotFoundException When User Is Not Found")
+    @Order(5)
+    public void shouldThrowExceptionWhenUserIsNotFound() {
+        String expectedMessage = "User with id 3 was not found";
+        String actualMessage = "";
+        Long userIdToDelete = 3l;
+
+        userRepository.deleteById(userIdToDelete);
+
+        try{
+            User user = userRepository.findById(userIdToDelete).orElseThrow(() ->
+                    new UserNotFoundException(String.format("User with id %s was not found", userIdToDelete)));
+        } catch (UserNotFoundException e) {
+            actualMessage = e.getMessage();
+        }
+
+        assertTrue(actualMessage.contains(expectedMessage), "User with id 3 is not present in database");
+    }
+
+    @Test
+    @DisplayName("After Create newUser There Should Be 6 Users In Database")
+    @Order(6)
+    public void afterCreateNewUsergetUsersShouldReturn6UsersTest() {
+        User newUser = User.builder()
+                .name("Boguś")
+                .email("bogus@example.com")
+                .build();
+
+        userRepository.save(newUser);
+        List<User> usersList = userRepository.findAll();
+        int expectedUserNumber = 6;
+
+        assertEquals(expectedUserNumber, usersList.size());
+    }
 }
