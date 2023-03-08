24fa59e3db831186d8b818718fd5dd3677143703
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-08 11:44:10 +0100

    logger to UserNotFoundException added

--- src/main/java/com/komfortcieplny/FakeEmailSender/user/exceptions/UserNotFoundException.java
+++ src/main/java/com/komfortcieplny/FakeEmailSender/user/exceptions/UserNotFoundException.java
@@ -1,8 +1,14 @@
 package com.komfortcieplny.FakeEmailSender.user.exceptions;
 
+import com.komfortcieplny.FakeEmailSender.user.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class UserNotFoundException  extends RuntimeException {
+    private static final Logger logger = LogManager.getLogger(UserService.class);
 
     public UserNotFoundException(String message) {
         super(message);
+        logger.error(message);
     }
 }
