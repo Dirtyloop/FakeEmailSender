0e5c7c10148e42bc00e9b17284d2fa5b110d7d6f
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-09 21:53:02 +0100

    RequestLoggerTest should contains logged message test added

--- src/test/java/com/komfortcieplny/FakeEmailSender/utils/RequestLoggerTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/utils/RequestLoggerTest.java
@@ -1,8 +1,14 @@
 package com.komfortcieplny.FakeEmailSender.utils;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +22,19 @@ class RequestLoggerTest {
         String message = "Test message";
 
         assertTrue(requestLogger.logInfo(message));
+    }
+
+    @Test
+    @DisplayName("Should Contains Logged Message")
+    void shouldContainsLoggedMessageTest() throws IOException {
+        String generatedString = RandomStringUtils.randomAlphabetic(16);
+        Path filePath = Path.of("./logs/requests.log");
+        String fileContent = Files.readString(filePath);
+
+        requestLogger.logInfo(generatedString);
+
+        assertTrue(Files.readString(filePath).contains(generatedString));
+
+        Files.writeString(filePath, fileContent, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
     }
 }
