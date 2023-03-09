9b62071035a6acee09ed0f0d4b7a22227478a4bf
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-03-09 21:54:18 +0100

    RequestLoggerTest should contains logged date and time test added

--- src/test/java/com/komfortcieplny/FakeEmailSender/utils/RequestLoggerTest.java
+++ src/test/java/com/komfortcieplny/FakeEmailSender/utils/RequestLoggerTest.java
@@ -3,6 +3,8 @@ package com.komfortcieplny.FakeEmailSender.utils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Contains;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -34,6 +36,21 @@ class RequestLoggerTest {
         requestLogger.logInfo(generatedString);
 
         assertTrue(Files.readString(filePath).contains(generatedString));
+
+        Files.writeString(filePath, fileContent, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+    }
+
+    @Test
+    @DisplayName("Should Contains Logged Date And Time")
+    void shouldContainsLoggedDateAndTimeTest() throws IOException {
+        String generatedString = RandomStringUtils.randomAlphabetic(16);
+        Path filePath = Path.of("./logs/requests.log");
+        String fileContent = Files.readString(filePath);
+        String dateTimeNow = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        requestLogger.logInfo(generatedString);
+
+        assertTrue(Files.readString(filePath).contains(dateTimeNow));
 
         Files.writeString(filePath, fileContent, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
     }
